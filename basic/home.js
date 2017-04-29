c3c47416e358989aae707e158298a526e597df2b
u4bi <u4bi@u4bi.com>
2017-04-29 21:30:40 +0900

    새창을 열시 이전 창 닫히게하는 로직

--- basic/home.js
+++ basic/home.js
@@ -6,7 +6,12 @@ var button = document.createElement('button')
 button.textContent = '창열기'
 
 button.addEventListener('click', () => {
-    main.openWindow('pageTwo')
+    var window = remote.getCurrentWindow() /* 현재 창을 window에 담음 */
+    
+    main.openWindow('pageTwo') /* 새창으로 열릴 페이지 */
+
+    window.close() // 이후 window 변수에 미리 정의받은 창을 닫는다
+
 }, false)
 
 document.body.appendChild(button)
